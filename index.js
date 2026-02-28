11505bbfd1f1e0b63cce9f4c96e7a6ed4610770f
SuryanshKumar001 <kumarsuryansh4747@gmail.com>
2026-02-28 17:17:41 +0530

    first

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let str="123";
 let convert=Number(str);
 console.log(convert);
+
---
+++ mid-level/problem.js
@@ -0,0 +1,11 @@
+let dae=new Date();
+console.log(dae);
+
+// let n=temporal.now.instant();
+// console.log(n);
+
+let date=temporal.plainDate(2024, 6, 1);
+console.log(date);
+console.log(date.year);
+console.log(date.month);
+console.log(date.day);
