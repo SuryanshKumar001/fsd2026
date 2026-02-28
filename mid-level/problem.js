959c6b33ef258b214af81114442808c60268a939
SuryanshKumar001 <kumarsuryansh4747@gmail.com>
2026-02-28 17:21:19 +0530

    one

--- mid-level/problem.js
+++ mid-level/problem.js
@@ -1,10 +1,10 @@
 let dae=new Date();
 console.log(dae);
 
-// let n=temporal.now.instant();
-// console.log(n);
+let n=temporal.now.instant();
+console.log(n);
 
-let date=temporal.plainDate(2024, 6, 1);
+let date=temporal.PlainDate.from({year: 2024, month: 6, day: 1});
 console.log(date);
 console.log(date.year);
 console.log(date.month);
